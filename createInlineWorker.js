b6ca7df2db16980a9481ff3c70ca2c5759ce78f5
Elton Muuga <emuuga@ea.com>
2015-03-03 14:43:18 +0100

    Attempt to use the Blob API first before falling back to deprecated alternatives

--- createInlineWorker.js
+++ createInlineWorker.js
@@ -5,13 +5,13 @@ module.exports = function(content, url) {
 	try {
 		try {
 			var blob;
-			try { // BlobBuilder = Deprecated, but widely implemented
+			try { // The proposed API
+				blob = new Blob([content], { type: 'application/javascript' });
+			} catch(e) { // BlobBuilder = Deprecated, but widely implemented
 				var BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder || window.MSBlobBuilder;
 				blob = new BlobBuilder();
 				blob.append(content);
 				blob = blob.getBlob();
-			} catch(e) { // The proposed API
-				blob = new Blob([content], { type: 'application/javascript' });
 			}
 			return new Worker(URL.createObjectURL(blob));
 		} catch(e) {
